c2b20745a9fd51ea8a3d665a38978a8c0c78ff89
dnguyen316 <duonglethai.nguyen2@dxc.com>
2024-10-07 10:30:53 +0700

    chore(JournalPage): update layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,21 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="h-screen w-screen relative">
+      <aside className="absolute top-0 left-0 h-full border-r w-[200px] border-black/10">
+        Mood
+      </aside>
+      <div className="ml-[200px]">
+        <header className="h-[60px] border-b border-black/10">
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
